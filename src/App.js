4dcc757def472effa924f9412d6f550aeffcf9d3
Simon Shepard <simon.shepard@gmail.com>
2020-08-17 11:37:12 +0200

    implemented window resize handler

--- src/App.js
+++ src/App.js
@@ -4,17 +4,38 @@ import Board from './board/';
 
 import {ThemeContext, themes} from './theme-context';
 
-function App() {
-  const width = window.innerWidth,
-  height = window.innerHeight;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: window.innerWidth,
+      height : window.innerHeight
+    };
+  }
 
-  return (
-    <ThemeContext.Provider value={themes.light}>
-      <div className="App">
-          <Board width={width} height={height} />
-      </div>
-    </ThemeContext.Provider>
-  );
+  handleWindowResize = () => {
+    this.setState({
+      width : window.innerWidth,
+      height : window.innerHeight
+    });
+  }
+
+  render() {
+    const { height, width } = this.state;
+
+    return (
+      <ThemeContext.Provider value={themes.light}>
+        <div className="App">
+            <Board width={width} height={height} />
+        </div>
+      </ThemeContext.Provider>
+    );
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
 }
 
 export default App;
