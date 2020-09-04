46a3b719da78e828e32daefe041fc682003c7ab5
Simon Shepard <simon.shepard@gmail.com>
2020-09-04 12:17:08 +0200

    forgot to commit utility file

---
+++ src/board/ui/tools/toolbar/utils.js
@@ -0,0 +1,29 @@
+export function createElementBaseObject(id, type, initialZoomLevel) {
+    return {
+        id,
+        type,
+        styles : {
+            x : 0,
+            y : 0,
+            width : 1,
+            height: 1,
+            fillOpacity: 0,
+            fill: "#ffffff",
+            stroke : "#000000",
+            strokeOpacity : 1,
+            strokeWidth : 2*initialZoomLevel,
+            strokeDasharray : "0"
+        },
+        fontStyle : {
+            fontSize : 24*initialZoomLevel,
+            fontFamily : "",
+            fontWeight : "normal",
+            fontStyle : "normal",
+            textDecorationLine : "",
+            color : "#080808",
+            textAlign: "center" 
+        },
+        text : "",
+        initialZoomLevel
+    };
+}
