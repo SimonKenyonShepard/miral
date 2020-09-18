5ff5b92fed19a0c7f9895374a0f1cc67820b1eaa
Simon Shepard <simon.shepard@gmail.com>
2020-09-18 10:39:58 +0200

    changed post-it colors

--- src/theme-context.js
+++ src/theme-context.js
@@ -3,11 +3,16 @@ import React from 'react';
 export const themes = {
   light: {
     preDefinedColors : [
-      "#FC72A6", //pink
-      "#1B8CA6", //blue
-      "#C8DE02", //green
-      "#EBE000", //yellow
-      "#F2A105" //orange
+      // "#FC72A6", //pink
+      // "#1B8CA6", //blue
+      // "#C8DE02", //green
+      // "#EBE000", //yellow
+      // "#F2A105" //orange
+      "#F28D95", //pink
+      "#7EBFD9", //blue
+      "#BBF244", //green
+      "#F2C744", //yellow
+      "#F29C50" //orange
     ],
     primaryColors : [
       "#ffffff", //white
