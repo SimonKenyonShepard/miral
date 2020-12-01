d353925b80a177e357e228d04a4ac18b4ad0fc99
Simon Shepard <simon.shepard@gmail.com>
2020-12-01 01:15:01 +0100

    added support for Monday.com hash values

--- src/board/ui/boardControls/share.js
+++ src/board/ui/boardControls/share.js
@@ -239,6 +239,13 @@ class Share extends Component {
                 companyName : organisation,
                 boardID
             });
+        } else if (window.location.hash.indexOf("boardId=") !== -1) {
+            const organisation = window.location.hash.match(/boardViewId=([^&$]*)/m)[1];
+            const boardID = window.location.hash.match(/boardId=([^&$]*)/m)[1];
+            this.setState({
+                companyName : organisation,
+                boardID
+            });
         }
     }
     
