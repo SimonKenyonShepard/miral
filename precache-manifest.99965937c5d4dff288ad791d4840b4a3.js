a58d4b296bc1c6659831354f8de8ae273053f60c
SimonKenyonShepard <SimonKenyonShepard@users.noreply.github.com>
2021-02-03 20:06:48 +0000

    deploy: d70d5fd11f41ec6f424dee2c8c446407501cee72

---
+++ precache-manifest.99965937c5d4dff288ad791d4840b4a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c3790bffa16fbeed87d4ab1ae6f0f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcad36c537b1b629e874",
+    "url": "/static/css/main.8823d4be.chunk.css"
+  },
+  {
+    "revision": "2be8a88d91c5d1de0ebd",
+    "url": "/static/js/2.cbe43bd6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cbe43bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcad36c537b1b629e874",
+    "url": "/static/js/main.0125669f.chunk.js"
+  },
+  {
+    "revision": "8cff9586ca1f750fdccf",
+    "url": "/static/js/runtime-main.77ba81e1.js"
+  }
+]);
